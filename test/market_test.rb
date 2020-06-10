5e3a3ad8a3d1bc5de2403cbb5a783960d1a40707
Kiera Allen <kieraaallen@gmail.com>
2020-06-10 09:41:25 -0600

    Add add_vendor method and test vendors collection after adding vendors

--- test/market_test.rb
+++ test/market_test.rb
@@ -8,7 +8,22 @@ class MarketTest < Minitest::Test
 
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
+
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @vendor3.stock(@item1, 65)
   end
 
   def test_it_exists
@@ -22,45 +37,16 @@ class MarketTest < Minitest::Test
   def test_that_it_stars_with_no_vendors
     assert_equal [], @market.vendors
   end
+
+  def test_it_can_add_and_list_vendors
+  @market.add_vendor(@vendor1)
+  @market.add_vendor(@vendor2)
+  @market.add_vendor(@vendor3)
+  assert_equal [@vendor1, @vendor2, @vendor3], @market.vendors
+  end
 end
 
-# pry(main)> item1 = Item.new({name: 'Peach', price: "$0.75"})
-# #=> #<Item:0x007f9c56740d48...>
-#
-# pry(main)> item2 = Item.new({name: 'Tomato', price: "$0.50"})
-# #=> #<Item:0x007f9c565c0ce8...>
-#
-# pry(main)> item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-# #=> #<Item:0x007f9c562a5f18...>
-#
-# pry(main)> item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-# #=> #<Item:0x007f9c56343038...>
-#
-# pry(main)> vendor1.stock(item1, 35)
-#
-# pry(main)> vendor1.stock(item2, 7)
-#
-# pry(main)> vendor2 = Vendor.new("Ba-Nom-a-Nom")
-# #=> #<Vendor:0x00007fe1349bed40...>
-#
-# pry(main)> vendor2.stock(item4, 50)
-#
-# pry(main)> vendor2.stock(item3, 25)
-#
-# pry(main)> vendor3 = Vendor.new("Palisade Peach Shack")
-# #=> #<Vendor:0x00007fe134910650...>
-#
-# pry(main)> vendor3.stock(item1, 65)
-#
-# pry(main)> market.add_vendor(vendor1)
-#
-# pry(main)> market.add_vendor(vendor2)
-#
-# pry(main)> market.add_vendor(vendor3)
-#
-# pry(main)> market.vendors
-# #=> [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe1349bed40...>, #<Vendor:0x00007fe134910650...>]
-#
+
 # pry(main)> market.vendor_names
 # #=> ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
 #
