468824595a4732e91ee8d8f9b1486d8fa9e9aab0
Kiera Allen <kieraaallen@gmail.com>
2020-06-10 09:14:36 -0600

    Create Vendor test file, test attributes and existence

---
+++ test/vendor_test.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_attributes
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+  end
+end
+
+# pry(main)> vendor.inventory
+# #=> {}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 0
+#
+# pry(main)> vendor.stock(item1, 30)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 30}
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 30
+#
+# pry(main)> vendor.stock(item1, 25)
+#
+# pry(main)> vendor.check_stock(item1)
+# #=> 55
+#
+# pry(main)> vendor.stock(item2, 12)
+#
+# pry(main)> vendor.inventory
+# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
+# ```
